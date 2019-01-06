812fc70a014eb14342dc8502b1e7c9c86286f5a6
Ozzie Gooen <Ozzie@grandroundshealth.com>
2019-01-06 13:28:38 +0000

    Removed unused packages

--- packages/gitnet-js/bundle.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function main() {
-  return 3;
-}
-
-module.exports = main;
--- packages/gitnet-js/src/main.js
+++
@@ -1,8 +0,0 @@
-import jsonFormatter from "./json-formatter"
-import {Database} from "./database"
-
-export default function main(data){
-    const statements = jsonFormatter(data);
-    let db = new Database(statements);
-    return db;
-}
