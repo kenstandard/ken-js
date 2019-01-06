fd352f23d00f3e5b19e006573bf7feffdc83e37e
Ozzie Gooen <Ozzie@grandroundshealth.com>
2019-01-06 19:57:12 +0000

    Modified siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  url: 'https://kenstandard.github.io/ken-js', // Your website URL
   baseUrl: '/', // Base URL for your project */
   projectName: 'ken-js',
   organizationName: 'kenstandard',
