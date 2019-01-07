1203ba4402ccb098a4b34728115f84ee40edf6e0
Ozzie Gooen <Ozzie@grandroundshealth.com>
2019-01-07 14:44:56 +0000

    Minor logo enhancements

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,8 +38,8 @@ const siteConfig = {
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/docusaurus.svg',
-  footerIcon: 'img/docusaurus.svg',
+  headerIcon: 'img/ken-logo-small.png',
+  footerIcon: 'img/ken-logo-small.png',
   favicon: 'img/favicon.png',
 
   /* Colors for website */
