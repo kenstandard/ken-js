d8fec3810aa6131d9b9c566f73a72379d5947b80
Ozzie Gooen <Ozzie@grandroundshealth.com>
2019-01-09 23:04:46 +0000

    Fixed small issue for compilation

--- packages/ken-explorer/src/instances/table.js
+++ packages/ken-explorer/src/instances/table.js
@@ -6,31 +6,32 @@ import { Table, Divider, Tag } from 'antd';
 // TODO: Fix for new gitCore
 export class TableShow extends Component {
   render() {
-    let {thingId} = this.props;
-    let columns = thing.inverseStatements().filter(s => s.propertyId === "p-column-of").map(r => r.thing())
-    let rows = thing.inverseStatements().filter(s => s.propertyId === "p-is-row-of").map(r => r.thing())
-    const _columns = columns.map(c => {
-        let property = c.propertyStatements("p-property")[0].findValueAsThing();
-        return ({
-            title: property.textValue("p-name"),
-            dataIndex: 'thing',
-            key: c.id,
-            render: (thing) => {
-                return <Value value={thing.formattedValue(property.id)}/>
-            }
-        })
-    })
-    let data = rows.map(row => ({thing: row}))
-    return (
-      <div>
-      <h1> Table! </h1>
-        <Table
-        columns={_columns}
-        dataSource={data}
-        size="small"
-        pagination={false}
-        />
-      </div>
-    );
+    return (<div>test</div>)
   }
 }
+    // let {thingId} = this.props;
+    // let columns = thing.inverseStatements().filter(s => s.propertyId === "p-column-of").map(r => r.thing())
+    // let rows = thing.inverseStatements().filter(s => s.propertyId === "p-is-row-of").map(r => r.thing())
+    // const _columns = columns.map(c => {
+    //     let property = c.propertyStatements("p-property")[0].findValueAsThing();
+    //     return ({
+    //         title: property.textValue("p-name"),
+    //         dataIndex: 'thing',
+    //         key: c.id,
+    //         render: (thing) => {
+    //             return <Value value={thing.formattedValue(property.id)}/>
+    //         }
+    //     })
+    // })
+    // let data = rows.map(row => ({thing: row}))
+    // return (
+    //   <div>
+    //   <h1> Table! </h1>
+    //     <Table
+    //     columns={_columns}
+    //     dataSource={data}
+    //     size="small"
+    //     pagination={false}
+    //     />
+    //   </div>
+    // );
